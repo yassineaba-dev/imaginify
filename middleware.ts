f634eeb70d1403b514d18d7d818162b088b241b6
yassineaba-dev <yassineaba.dev@gmail.com>
2025-09-04 01:34:34 +0100

    first commit

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,17 @@
 import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
-  publicRoutes: ["/", "/api/webhooks/clerk", "/api/webhooks/stripe"],
-  ignoredRoutes: ["/api/webhooks/clerk", "/api/webhooks/stripe"],
+  // Skip Clerk auth checks for webhooks
+  ignoredRoutes: [
+    "/api/webhooks/clerk",
+    "/api/webhooks/stripe",
+  ],
 });
 
 export const config = {
-  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: [
+    "/((?!.+\\.[\\w]+$|_next).*)",
+    "/",
+    "/(api|trpc)(.*)",
+  ],
 };
